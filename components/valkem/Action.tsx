9c39825de27e93f4a0accce56072a677e9817870
Emmanuel Gbidi <92477145+Saintholi@users.noreply.github.com>
2025-08-03 00:13:49 +0000

    Update Action.tsx

--- components/valkem/Action.tsx
+++ components/valkem/Action.tsx
@@ -6,9 +6,9 @@ const Action = () => {
       {/* <Link href="mailto:info@tanfreight.com">info@tanfreight.com</Link> */}
       <a
         className="flex items-center justify-end rounded-lg border bg-gray-500 p-2 text-white"
-        href="tel:+233 204 261 569"
+        href="tel:+233203333906"
       >
-        +233 204 261 569
+        +233 203 333 906
       </a>
     </>
   );
